19dd001b0f25bb2432a6a1ff899a981c488afce0
Gabriel LANDAIS <gabriel.landais@kdecole.fr>
2014-02-24 10:51:24 +0100

    Query constructor with EntityManager provided

--- src/main/resources/genQueryBody.java
+++ src/main/resources/genQueryBody.java
@@ -3,6 +3,10 @@
 		super("this", new HQLQueryBuilder(TYPE.class));
 	}
 
+	public CLASSNAME(EntityManager em) {
+		super("this", new HQLQueryBuilder(em, TYPE.class));
+	}
+
 	public CLASSNAME(HQLQueryBuilder<?> queryBuilder) {
 		super("this", queryBuilder);
 	}
