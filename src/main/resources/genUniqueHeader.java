f6276e9357ed36ce6e1a6e9e849d3b510ef7ff0f
Gabriel Landais <gabriel.landais@gmail.com>
2021-10-23 22:13:08 +0200

    Fix

--- src/main/resources/genUniqueHeader.java
+++ src/main/resources/genUniqueHeader.java
@@ -1,5 +1,8 @@
-import io.github.glandais.hql.HQLQueryBuilderInterface;
+import java.util.*;
 import io.github.glandais.hql.*;
+import io.github.glandais.hql.beans.*;
+import io.github.glandais.hql.paths.*;
+import io.github.glandais.hql.restrictions.*;
 
 @SuppressWarnings("all")
 public class CLASSNAME<T extends TYPE> extends SUPER<T> implements HQLQueryBuilderInterface<T> {
